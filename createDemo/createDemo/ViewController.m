7fd1fb2c934d42b9896be169881966ed2f61b369
huyiyong <huyiyong@jd.com>
2018-07-29 22:30:24 +0800

    创建方法

--- createDemo/createDemo/ViewController.m
+++ createDemo/createDemo/ViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self addUI];
+}
+#pragma mark
+-(void)addUI{
+    
 }
 
 
